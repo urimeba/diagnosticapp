13c02ea7016857229d5962999fc21054de4818c4
Uriel Medina <urimeba511@gmail.com>
2020-11-24 10:58:28 -0600

    *fix* Debug at settings

--- diagnosticapp/settings.py
+++ diagnosticapp/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
